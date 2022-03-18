0e0110649cd640277d8d1e8a43e523a145026fb3
Luis Yosa <lfyossag@gmail.com>
2022-03-18 23:28:44 +0000

    Formulario de registro terminado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../styles/home.css";
 import LoginForm from "../component/formLogin";
-import FormRegister from "../component/formRegister";
 import Jumbotron from "../component/Jumbotron";
 
 
@@ -9,7 +8,6 @@ const Home = () => {
 	return(
 		<div className="text-center mt-5">
 		<Jumbotron/>
-		<FormRegister/>
 		</div>
 	)
 	
