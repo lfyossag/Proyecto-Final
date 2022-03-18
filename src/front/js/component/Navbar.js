9400613a304d191b71330046929da2597ed79e33
David1997sb <salasbar97@gmail.com>
2022-03-18 23:29:16 +0000

    search

--- src/front/js/component/Navbar.js
+++ src/front/js/component/Navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
     <a class="navbar-brand">Navbar</a>
     <form class="d-flex">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      <div class="btn btn-outline-success" >Search</div>
     </form>
   </div>
 </nav>
