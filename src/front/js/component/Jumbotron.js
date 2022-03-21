1548dda788ead5045080e6ab31fa19182a15fde1
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-03-21 23:16:02 +0000

    rutas dinamicas

--- src/front/js/component/Jumbotron.js
+++ src/front/js/component/Jumbotron.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Jumbotron = () => {
   return (
-    <div className="card" style={{width: "18rem;"}}>
+    <div className="card" style={{width: "18rem"}}>
   <img src="..." className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
