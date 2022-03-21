3e9df9aa74660cc9450a6679216f1fe4942f6a02
aseg3092 <gabrielsalazar3092@gmail.com>
2022-03-21 04:19:40 +0000

    loginFE integrado con navBar

--- src/front/js/component/NavBarGabriel.js
+++ src/front/js/component/NavBarGabriel.js
@@ -1,48 +1,64 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import "../../styles/nav.css";
+
 
 export const NavbarGabriel = () => {
-    const { store, actions } = useContext(Context)
-    return (
-        <nav class="navbar navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand">Navbar</a>
-
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        Icono
+	const {store, actions} = useContext(Context)
+	return (
+	
+	<nav className="navbar navbar-expand-lg navbar-light bg-light text-muted bg-dark">
+	<div className="container">
+				<a className="navbar-brand text-white">
+					<img
+						alt="svgImg"
+						src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4Igp3aWR0aD0iNDgiIGhlaWdodD0iNDgiCnZpZXdCb3g9IjAgMCA0OCA0OCIKc3R5bGU9IiBmaWxsOiMwMDAwMDA7Ij48cGF0aCBmaWxsPSIjNjRiNWY2IiBkPSJNMzcsMzZIMTdjLTIuNzYxLDAtNS0yLjIzOS01LTVWMTFjMC0yLjc2MSwyLjIzOS01LDUtNWgyMGMyLjc2MSwwLDUsMi4yMzksNSw1djIwIEM0MiwzMy43NjEsMzkuNzYxLDM2LDM3LDM2eiBNMjEsMzBoMTRWMTguNWMwLTMuMDM4LTIuNDYyLTUuNS01LjUtNS41SDE4djE0QzE4LDI4LjY1NywxOS4zNDMsMzAsMjEsMzB6Ij48L3BhdGg+PHBhdGggZmlsbD0iI2ZiOGMwMCIgZD0iTTMxLDQySDExYy0yLjc2MSwwLTUtMi4yMzktNS01VjE3YzAtMi43NjEsMi4yMzktNSw1LTVoMjBjMi43NjEsMCw1LDIuMjM5LDUsNXYyMCBDMzYsMzkuNzYxLDMzLjc2MSw0MiwzMSw0MnogTTE3LjUsMzZIMzBWMjFjMC0xLjY1Ny0xLjM0My0zLTMtM0gxNWMtMS42NTcsMC0zLDEuMzQzLTMsM3Y5LjVDMTIsMzMuNTM4LDE0LjQ2MiwzNiwxNy41LDM2eiI+PC9wYXRoPjxwYXRoIGZpbGw9IiM2NGI1ZjYiIGQ9Ik0zNiwyNnYxYzAsMS42NTctMS4zNDMsMy0zLDNIMjN2NmgxNGMyLjc1LDAsNS0yLjI1LDUtNXYtNUgzNnoiPjwvcGF0aD48L3N2Zz4="
+						/>
+						FIERAS
+				</a>
+		<div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+			<ul className="navbar-nav topButtons ">
+				<li className="nav-item me-5 ">
+				<a className="d-flex justify-content-center nav-link active text-white border rounded-pill navigationButtons" aria-current="page" href="#">WOMEN</a>
+				</li>
+				<li className="nav-item ms-5">
+				<a className="d-flex justify-content-center nav-link active text-white border rounded-pill navigationButtons" aria-current="page" href="#">MEN</a>
+				</li>
+			</ul>
+    	</div>
+		<form className="d-flex topIcons ">
+        <div className="dropdown  ">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownLoginUsuario" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i className="bi bi-person-circle  loginIcon "></i>
                     </button>
-                    {/* <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1"> */}
-                    <form class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-
-                        <div class="container">
-                            <div class="row">
-                                <div class="col">
-                                    Column
-                                </div>
-                                <div class="col">
-                                    Column
-                                </div>
-                                <div class="col">
-                                    Column
-                                </div>
+                    <div className="dropdown-menu " aria-labelledby="dropdownLoginUsuario">
+                        <div className="d-flex">
+                            <span>Sing in</span>
+                            <a className="" href="#">Sing up</a>
+                        </div>
+                        <form >
+                            <div className="form-floating ">
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label for="floatingInput">Email</label>
                             </div>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                            <label for="floatingInput">Email address</label>
-                        </div>
-                        <div class="form-floating">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                            <label for="floatingPassword">Password</label>
-                        </div>
-
-                        <button type="submit" class="btn btn-secondary">Login</button>
-                    </form>
+                            <div className="form-floating">
+                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                                <label for="floatingPassword">Password*</label>
+                            </div>
+                        </form>
+                        <div className="invisible">
+                             <a href="#">You forgot,right?</a>
+                        </div> 
+                        <div >
+                                <button type="submit" className="btn btn-secondary">Login</button>
+                        </div> 
+                    </div>  
                 </div>
-            </div>
-        </nav>
-    );
+			<i className="bi bi-cart-check-fill"></i>
+		</form>
+	</div>
+	</nav>
+	);
 };
 export default NavbarGabriel;
