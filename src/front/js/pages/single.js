16a484462563224f50d8c29015ecf749bf5e310d
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-04-01 03:32:19 +0000

    single simplificado

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -20,74 +20,82 @@ export const Single = props => {
     // 		</span>
     // 	</Link>
     // </div>
-      <div className="row container">
-        <div className="col-lg-8">
-          <div
-            className="ecommerce-gallery"
-            data-mdb-zoom-effect="true"
-            data-mdb-auto-height="true"
-          >
-            <div className="row py-3 shadow-5">
-              <div className="col-12 mb-1">
-                <div className="lightbox">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
-                    alt="Gallery image 1"
-                    className="ecommerce-gallery-main-img active w-100"
-                  />
-                </div>
-              </div>
-              <div className="col-3 mt-1">
+    <div className="row container">
+      <div className="col-lg-8">
+        <div
+          className="ecommerce-gallery"
+          data-mdb-zoom-effect="true"
+          data-mdb-auto-height="true"
+        >
+          <div className="row py-3 shadow-5">
+            <div className="col-12 mb-1">
+              <div className="lightbox">
                 <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
-                  data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
+                  src={store.item.image}
                   alt="Gallery image 1"
-                  className="active w-100"
-                />
-              </div>
-              <div className="col-3 mt-1">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
-                  data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
-                  alt="Gallery image 2"
-                  className="w-100"
-                />
-              </div>
-              <div className="col-3 mt-1">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
-                  data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
-                  alt="Gallery image 3"
-                  className="w-100"
-                />
-              </div>
-              <div className="col-3 mt-1">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/15a.webp"
-                  data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/15a.webp"
-                  alt="Gallery image 4"
-                  className="w-100"
+                  className="ecommerce-gallery-main-img active w-100"
                 />
               </div>
             </div>
-          </div>
-        </div>
-        <div className="col-lg-4 my-3 mx-auto">
-          <div className="card h-100">
-            <h5 className="card-header">Product details</h5>
-            <div className="card-body">
-              <h5 className="card-title">{store.item.title}</h5>
-              <p className="card-text">
-               $ {store.item.price} : 
-              </p>
-			  <span>item{store.item.id}</span>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
+            <div className="col-3 mt-1">
+              <img
+                src={store.item.image}
+                data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
+                alt="Gallery image 1"
+                className="active w-100"
+              />
+            </div>
+            <div className="col-3 mt-1">
+              <img
+                src={store.item.image}
+                data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
+                alt="Gallery image 2"
+                className="w-100"
+              />
+            </div>
+            <div className="col-3 mt-1">
+              <img
+                src={store.item.image}
+                data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
+                alt="Gallery image 3"
+                className="w-100"
+              />
+            </div>
+            <div className="col-3 mt-1">
+              <img
+                src={store.item.image}
+                data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/15a.webp"
+                alt="Gallery image 4"
+                className="w-100"
+              />
             </div>
           </div>
         </div>
       </div>
+      <div className="col-lg-4 my-3 mx-auto">
+        <div className="card h-100 text-center">
+          <h5 className="card-header">Product details</h5>
+          <div className="card-body">
+            <h5 className="card-title">{store.item.title}</h5>
+            <p className="card-text">$ {store.item.price}</p>
+            <p>item {store.item.id}</p>
+            <span>Units</span>
+            <div class="input-group input-group-sm mb-3">
+              <input
+                type="text"
+                class="form-control"
+                title="Untis"
+                aria-label="Example text with button addon"
+                aria-describedby="button-addon1"
+              ></input>
+            </div>
+            <div className="add-to-cart">
+              <button className="btn btn-dark">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
